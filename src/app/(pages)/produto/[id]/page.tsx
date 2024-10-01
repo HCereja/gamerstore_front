c1ecc433a315af7f3d8a3a57850b997c2ab084ae
Henrique <cerejahenrique@gmail.com>
2024-10-01 20:49:26 -0300

    feat: update product page

--- src/app/(pages)/produto/[id]/page.tsx
+++ src/app/(pages)/produto/[id]/page.tsx
@@ -1,10 +1,22 @@
+import ProductInfo from "@/components/product/ProductInfo";
+import ProductNotFound from "@/components/product/ProductNotFound";
+import ProductTitle from "@/components/product/ProductTitle";
 import { products } from "@/core";
 
 const ProductPage = (props: any) => {
   const id = +props.params.id;
   const product = products.find((prod) => prod.id === id);
 
-  return <div>{product?.name || "Produto"}</div>;
+  return product ? (
+    <div className="flex flex-col gap-20 container py-10">
+      <div className="flex flex-col gap-10">
+        <ProductTitle product={product} />
+        <ProductInfo product={product} />
+      </div>
+    </div>
+  ) : (
+    <ProductNotFound />
+  );
 };
 
 export default ProductPage;
